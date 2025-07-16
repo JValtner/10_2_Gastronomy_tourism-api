8e85b22a57d19f112e5a0caf8a0e7f381ae40189
Josip Valtner <josipvaltner@gmail.com>
2025-07-16 19:04:43 +0200

    FeatFeatReservations

--- src/tourism-api/Controllers/TourReservationController.cs
+++ src/tourism-api/Controllers/TourReservationController.cs
@@ -4,14 +4,56 @@ using tourism_api.Repositories;
 
 namespace tourism_api.Controllers
 {
-    [Route("api/tour_reservations")]
+    [Route("api/tour-reservations")]
     [ApiController]
     public class TourReservationController : ControllerBase
     {
         private readonly TourReservationRepository _tourReservationRepo;
+        private readonly UserRepository _userRepo;
+        private readonly TourRepository _tourRepo;
         public TourReservationController(IConfiguration configuration)
         {
             _tourReservationRepo = new TourReservationRepository(configuration);
+            _userRepo = new UserRepository(configuration);
+            _tourRepo = new TourRepository(configuration);
+        }
+        [HttpGet("user/{userId}")]
+        public ActionResult<List<TourReservations>> GetByUserId(int userId)
+        {
+            try
+            {
+                User user = _userRepo.GetById(userId);
+                if (user == null)
+                {
+                    return NotFound($"Korisnik sa ID {userId} nije pronađen.");
+                }
+
+                List<TourReservations> reservations = _tourReservationRepo.GetByUserId(userId);
+                return Ok(reservations);
+            }
+            catch (Exception ex)
+            {
+                return Problem("Greška pri učitavanju rezervacija korisnika.");
+            }
+        }
+        [HttpGet("tour/{tourId}")]
+        public ActionResult<List<TourReservations>> GetByTourId(int tourId)
+        {
+            try
+            {
+                Tour tour = _tourRepo.GetById(tourId);
+                if (tour == null)
+                {
+                    return NotFound($"Tura sa ID {tourId} nije pronađen.");
+                }
+
+                List<TourReservations> reservations = _tourReservationRepo.GetByUserId(tourId);
+                return Ok(reservations);
+            }
+            catch (Exception ex)
+            {
+                return Problem("Greška pri učitavanju rezervacija korisnika.");
+            }
         }
 
         [HttpPost]
@@ -21,7 +63,12 @@ namespace tourism_api.Controllers
             {
                 return BadRequest("Invalid Reservation data.");
             }
-
+            //check available space for new reservation
+            if (!_tourReservationRepo.CheckAvailableSpace(tourReservation.TourId, tourReservation.NumberOfGuests))
+            {
+                return Conflict("Tour is fully booked, no available space.");
+            }
+            
             try
             {
                 TourReservations createdTourReservation = _tourReservationRepo.Create(tourReservation);
