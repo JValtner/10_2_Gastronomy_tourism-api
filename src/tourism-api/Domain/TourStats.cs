68a2e102c11b9689899a8261ebc74275541ac41a
Josip Valtner <josipvaltner@gmail.com>
2025-08-27 11:38:36 +0200

    Feat Statistike, dodati statisticki prikazi za vodice i integrisane mape

---
+++ src/tourism-api/Domain/TourStats.cs
@@ -0,0 +1,11 @@
+﻿namespace tourism_api.Domain
+{
+    public class TourStats
+    {
+        public List<Tour> tourMaxRes { get; set; } = new List<Tour>();
+        public List<Tour> tourMinRes { get; set; } = new List<Tour>();
+        public List<Tour> tourMaxPercentage { get; set; } = new List<Tour>();
+        public List<Tour> tourMinPercentage { get; set; } = new List<Tour>();
+        public int totalTours { get; set; } = 0;
+    }
+}
